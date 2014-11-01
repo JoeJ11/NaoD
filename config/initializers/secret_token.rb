7be25c44d6e1965016ca77d7a930adb69811354a
JU An <andyjvan@gmail.com>
2014-11-01 15:46:28 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NaoD::Application.config.secret_key_base = '0e2431058f49cfd4b728ecf55f1d67f9043210ab41ee4b50f1f34cd230c89340b143586af54fb5c8df75762ebaed4bcdd113f304ba098203abb76f991d321140'
